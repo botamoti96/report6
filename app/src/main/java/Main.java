4c5d1016474ecc1f409d3b16a158ba91947ff399
botamoti96 <higak@DESKTOP-T97A0UI.localdomain>
2021-01-08 16:53:23 +0900

    complete!

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main{
     public static void main(String[] args){
         GameMaster gm = new GameMaster();
         gm.gameSetup();
-        gm.setRange();
+        //gm.setRange();
         gm.game();
     }
 }
